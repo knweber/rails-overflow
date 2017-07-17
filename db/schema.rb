c8d63f4ebaf92bce394dc62a5d801103bd7e31d4
Joshua Rudio & Kris <pair+JoshuaRudio.knweber@devbootcamp.com>
2017-07-17 15:33:12 -0700

    Add Answer model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717222733) do
+ActiveRecord::Schema.define(version: 20170717223115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "question_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "prompt", null: false
@@ -34,5 +44,7 @@ ActiveRecord::Schema.define(version: 20170717222733) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
