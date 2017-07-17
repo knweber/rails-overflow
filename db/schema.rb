0c811116475ae223e7736941188445b8a35e0c83
Joshua Rudio & Kris <pair+JoshuaRudio.knweber@devbootcamp.com>
2017-07-17 15:30:28 -0700

    Add Question model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717175725) do
+ActiveRecord::Schema.define(version: 20170717222733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "prompt", null: false
+    t.text "description", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170717175725) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
