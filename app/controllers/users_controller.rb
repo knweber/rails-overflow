7035c0ea391bf71b154a434723c4c6722d8415b8
Joshua Rudio & Kris <pair+JoshuaRudio.knweber@devbootcamp.com>
2017-07-17 15:47:42 -0700

    Add user_params method to UsersHelper and build #new and #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  include UsersHelper
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'root'
+    else
+      render 'new'
+    end
+  end
 end
