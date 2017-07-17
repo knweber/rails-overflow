b86611825cf714b8bfa0d67b44fadd2ba1b028c6
Joshua Rudio & Kris <pair+JoshuaRudio.knweber@devbootcamp.com>
2017-07-17 15:59:00 -0700

    Add BCrypt, build User#new view with form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render 'root'
+      render 'home/index'
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
-class User < ApplicationRecord
+class User < ApplicationRecord  
+  validates :username, :email, :password, { presence: true }
+
+  has_many :questions
+  has_many :answers
+
+  has_secure_password
 end
