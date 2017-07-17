f9fe36c088988dab26849190b93853b3d77f996c
Joshua Rudio & Kris <pair+JoshuaRudio.knweber@devbootcamp.com>
2017-07-17 15:41:58 -0700

    Add controllers for Question and Answer and open routes including nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  resources :users, except: :index do
+    resources  :questions
+    resources  :answers
+  end
+  resources :questions do
+    resources :answers
+  end
+  resources :answers, except: [:index, :show]
+
   root 'home#index'
 end
